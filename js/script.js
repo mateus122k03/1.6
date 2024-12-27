3eb1290d331d97e6d8c2eb7fc3dadbd4a98514a7
mateus122k03 <mateuspessoal122k03@gmail.com>
2024-12-27 03:07:44 -0300

    arquivos 1.6

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('showPhotosBtn').addEventListener('click', function() {
+    const gallery = document.getElementById('photoGallery');
+    gallery.classList.toggle('hidden');
+});
